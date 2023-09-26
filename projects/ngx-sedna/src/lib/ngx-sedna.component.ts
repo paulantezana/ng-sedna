c17d9bfe47d7ca908ec00797badb8a2ea827dd79
Paul Antezana <PAntezana@grupovalor.com>
2023-09-26 18:34:31 -0500

    iniciando

---
+++ projects/ngx-sedna/src/lib/ngx-sedna.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sn-button',
+  template: `
+    <p>
+      ngx-sedna works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SnButtonComponent {
+
+}
---
+++ projects/ngx-sedna/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-sedna
+ */
+
+export * from './lib/ngx-sedna.component';
