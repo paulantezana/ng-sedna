c7b0b2ed5ff645e1ed52d984d76811f13cc61df0
Paul Antezana <PAntezana@grupovalor.com>
2024-01-30 18:22:45 -0500

    new updates

--- projects/ngx-sedna/data-table/data-table/data-table.component.ts
+++ projects/ngx-sedna/data-table/data-table/data-table.component.ts
@@ -33,15 +33,6 @@ export class SnDataTableComponent {
 
   @Output() readonly snQueryParams = new EventEmitter<SnDataTableQueryParams>();
 
-  // [pageSizeOptions]="[10,25,50,100,250,500,1000]"
-  // [length]="paginate.totalLength"
-  // [pageSize]="paginate.pageSize"
-  // [pageIndex]="paginate.pageIndex"
-  // (pageChange)="onPageChange($event)"
-
-  // snFiler
-
-
   ngOnInit(): void {
     const { pageIndexDistinct$, pageSizeDistinct$, queryParams$, filterDistinct$ } = this.snDataTableService;
 
@@ -50,19 +41,16 @@ export class SnDataTableComponent {
     pageIndexDistinct$.pipe(takeUntil(this.destroy$)).subscribe(pageIndex => {
       if (pageIndex !== this.snPageIndex) {
         this.snPageIndex = pageIndex;
-        // this.nzPageIndexChange.next(pageIndex);
       }
     });
 
     pageSizeDistinct$.pipe(takeUntil(this.destroy$)).subscribe(pageSize => {
       if (pageSize !== this.snPageSize) {
         this.snPageSize = pageSize;
-        // this.snPageSizeChange.next(pageSize);
       }
     });
 
     filterDistinct$.pipe(takeUntil(this.destroy$)).subscribe(filter => {
-      console.log({ filter, KLT: this.snFilter }, 'ME_DISPARE_DESDE_LA_TABLA');
       if (JSON.stringify(filter) !== JSON.stringify(this.snFilter)) {
         this.snFilter = filter;
       }
@@ -70,7 +58,7 @@ export class SnDataTableComponent {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const { snColumns, snPageSize } = changes;
+    const { snColumns, snPageSize, snFilter } = changes;
 
     if (snColumns) {
       this.snColumns = this.snColumns || [];
@@ -80,10 +68,13 @@ export class SnDataTableComponent {
     if(snPageSize){
       this.snDataTableService.updatePageSize(this.snPageSize);
     }
+
+    if(snFilter){
+      this.snDataTableService.updateFilter(this.snFilter);
+    }
   }
 
   onFilterChange(filter: SnFilter[]) {
-    console.log(filter, 'CAMBIO_TABLA');
     this.snDataTableService.updateFilter(filter);
   }
 
--- projects/ngx-sedna/filter/public-api.ts
+++ projects/ngx-sedna/filter/public-api.ts
@@ -1,3 +1,4 @@
+export * from './filter.module';
 export * from './filter.types';
 export * from './filter.component';
-export * from './filter.module';
+export * from './filter.service';
